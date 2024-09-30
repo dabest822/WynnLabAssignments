47bfdc01cb1bf73194787767f9d29eae5867720c
dabest822 <dembrosdoe@gmail.com>
2024-09-30 18:48:09 -0400

    sept 30

--- lab-1-wynn-casadona/Gibdo Everything.gd
+++ lab-1-wynn-casadona/Gibdo Everything.gd
@@ -2,14 +2,13 @@ extends CharacterBody2D
 
 var speed = 50
 var detection_radius = 1000
-var attack_range = 50
 var gravity = 1200
+var attack_range = 30  # Reduced attack range
 var facing_direction = "right"
 var animation_player: AnimationPlayer
 var link: Node2D
-var ground_y_position: float
 var is_attacking = false
-var attack_cooldown = 4.0  # Fixed 4-second cooldown
+var attack_cooldown = 1.0  # 1 second cooldown between attacks
 var attack_timer = 0.0
 
 func _ready():
@@ -17,15 +16,13 @@ func _ready():
 	animation_player = find_animation_player(self)
 	if animation_player:
 		print("AnimationPlayer found at path: ", animation_player.get_path())
+		print("Available animations: ", animation_player.get_animation_list())
 	else:
 		print("ERROR: AnimationPlayer not found in the scene tree!")
-	link = get_node("/root/Node2D/Node2D/CollisionShape2D/Link")
-	if link:
-		print("Link found at path: ", link.get_path())
-	else:
-		print("ERROR: Link not found in the scene!")
 	
-	ground_y_position = global_position.y
+	link = get_node("/root/Node2D/Node2D/CollisionShape2D/Link")
+	if not link:
+		print("ERROR: Link not found in the scene!")
 
 func find_animation_player(node):
 	for child in node.get_children():
@@ -37,72 +34,72 @@ func find_animation_player(node):
 	return null
 
 func _physics_process(delta):
+	if not animation_player:
+		print("ERROR: AnimationPlayer is still null in _physics_process")
+		return
+	
 	apply_gravity(delta)
-	handle_movement(delta)
+	handle_movement_and_attack(delta)
 	move_and_slide()
 	update_animation()
-	print("Gibdo position: ", global_position)  # Debug print
 
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	else:
 		velocity.y = 0
-		global_position.y = ground_y_position
 
-func handle_movement(delta):
+func handle_movement_and_attack(delta):
 	if not link:
-		print("ERROR: Link not found in handle_movement")
 		return
-
+	
+	# Calculate distance and direction from Gibdo to Link
 	var to_link = link.global_position - global_position
-	to_link.y = 0  # Ignore vertical distance
 	var distance_to_link = to_link.length()
 	
-	print("Distance to Link: ", distance_to_link)  # Debug print
+	# Update facing direction based on Link's relative position
+	facing_direction = "right" if to_link.x > 0 else "left"
 
-	if distance_to_link <= attack_range:
-		if attack_timer <= 0:
-			# Attack Link
-			is_attacking = true
-			attack_timer = attack_cooldown
-			print("Gibdo is attacking! Next attack in 4 seconds.")
-			velocity.x = 0  # Stop moving while attacking
-		else:
-			# Cooldown active, don't move
-			velocity.x = 0
-	elif distance_to_link <= detection_radius:
-		# Move towards Link
-		var direction = to_link.normalized()
-		velocity.x = direction.x * speed
-		facing_direction = "right" if velocity.x > 0 else "left"
-		is_attacking = false
-		print("Moving towards Link. Velocity: ", velocity)  # Debug print
-	else:
-		velocity.x = 0
-		is_attacking = false
-	
 	# Update attack timer
 	if attack_timer > 0:
 		attack_timer -= delta
 
+	# Check if Gibdo should attack
+	if distance_to_link <= attack_range and attack_timer <= 0:
+		is_attacking = true
+		attack_timer = attack_cooldown
+		velocity = Vector2.ZERO  # Stop movement when attacking
+	else:
+		is_attacking = false
+		# Move towards Link if not in attack range but within detection radius
+		if distance_to_link <= detection_radius:
+			velocity.x = sign(to_link.x) * speed
+		else:
+			velocity.x = 0
+
+	print("Distance to Link: ", distance_to_link, " Velocity: ", velocity, " Facing: ", facing_direction, " Attacking: ", is_attacking, " Attack timer: ", attack_timer)
+
 func update_animation():
+	if not animation_player:
+		print("ERROR: Cannot update animation, AnimationPlayer is null!")
+		return
+	
 	var anim_to_play = ""
-	if is_attacking:
+
+	# Determine which animation to play based on Gibdo's state
+	if is_attacking and attack_timer > (attack_cooldown - 0.1):  # Ensure attack only starts once per cooldown
 		anim_to_play = "Attack_" + facing_direction.capitalize()
-	elif velocity.x != 0:
+	elif abs(velocity.x) > 0:
 		anim_to_play = "Walk_" + facing_direction.capitalize()
 	else:
 		anim_to_play = "Idle_" + facing_direction.capitalize()
 	
-	play_animation(anim_to_play)
-
-func play_animation(anim_name):
-	if animation_player != null:
-		if animation_player.has_animation(anim_name):
-			if animation_player.current_animation != anim_name:
-				animation_player.play(anim_name)
-		else:
-			print("WARNING: Animation '", anim_name, "' not found!")
+	# Only play animation if it's different from the current one
+	if animation_player.has_animation(anim_to_play):
+		if animation_player.current_animation != anim_to_play or not animation_player.is_playing():
+			animation_player.play(anim_to_play)
+			print("Playing new animation: ", anim_to_play)
 	else:
-		print("ERROR: Attempted to play animation '", anim_name, "' but AnimationPlayer is null!")
+		print("WARNING: Animation not found: ", anim_to_play)
+
+	print("Final animation state - Current: ", animation_player.current_animation, " Is playing: ", animation_player.is_playing())
