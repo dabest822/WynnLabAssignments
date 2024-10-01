0e6dfdf2053ee1cbc58d5a0a2f32f072e737717c
dabest822 <dembrosdoe@gmail.com>
2024-10-01 04:56:24 -0400

    10.1

--- lab-1-wynn-casadona/Link Everything.gd
+++ lab-1-wynn-casadona/Link Everything.gd
@@ -1,117 +1,93 @@
 extends CharacterBody2D
 
-# Player attributes
-var speed = 200  # Walking speed
-var run_speed = 400  # Running speed
-var gravity = 1200  # Gravity force to pull the player down
-var jump_force = -600  # Jump force to propel the player upwards
-
-# State Variables
-var is_jumping = false
-var is_running = false
-var in_air = false
-var is_attacking = false
+var speed = 200
+var run_speed = 400
+var jump_force = -600
+var gravity = 1200
 var facing_direction = "right"
-
-# Reference to AnimationPlayer for Link
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
+var is_attacking = false
+var is_jumping = false
+var animation_player: AnimationPlayer
 
 func _ready():
 	print("Link script starting...")
+	animation_player = find_animation_player(self)
+	
 	if animation_player:
 		print("AnimationPlayer found at path: ", animation_player.get_path())
-		# Connect signal for animation completion
+		# Connect the "animation_finished" signal correctly
 		animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
 	else:
-		print("ERROR: AnimationPlayer not found!")
+		print("ERROR: AnimationPlayer not found in the scene tree!")
+
+func find_animation_player(node):
+	for child in node.get_children():
+		if child is AnimationPlayer:
+			return child
+		var found = find_animation_player(child)
+		if found:
+			return found
+	return null
 
 func _physics_process(delta):
-	# Prevent movement during attacks
-	if not is_attacking:
-		handle_movement(delta)
+	handle_input(delta)
 	apply_gravity(delta)
-	apply_movement()
+	move_and_slide()
+	update_animation()
 
-	# Handle attack input separately
-	if Input.is_action_just_pressed("attack"):
-		handle_attack()
+func handle_input(_delta):
+	var current_speed = run_speed if Input.is_key_pressed(KEY_SHIFT) else speed
 
-# Handle player movement and jumping
-func handle_movement(_delta):
-	is_running = Input.is_action_pressed("run")
-	var current_speed = run_speed if is_running else speed
+	if not is_attacking:
+		velocity.x = 0
+		if Input.is_action_pressed("ui_right"):
+			velocity.x = current_speed
+			facing_direction = "right"
+		elif Input.is_action_pressed("ui_left"):
+			velocity.x = -current_speed
+			facing_direction = "left"
 
-	# Reset horizontal velocity
-	velocity.x = 0
-
-	# Walk or run left or right
-	if Input.is_action_pressed("ui_right"):
-		velocity.x = current_speed
-		facing_direction = "right"
-		if in_air:
-			play_animation("Jump_Right")
-		else:
-			play_animation("Run_Right" if is_running else "Walk_Right")
-	elif Input.is_action_pressed("ui_left"):
-		velocity.x = -current_speed
-		facing_direction = "left"
-		if in_air:
-			play_animation("Jump_Left")
-		else:
-			play_animation("Run_Left" if is_running else "Walk_Left")
-	else:
-		# Play idle animation if not moving
-		if not in_air:
-			play_animation("Idle_" + facing_direction.capitalize())
-
-	# Jumping
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_attacking:
 		velocity.y = jump_force
 		is_jumping = true
-		in_air = true
-		play_animation("Jump_" + facing_direction.capitalize())
 
-# Apply gravity while not on the ground
+	if Input.is_action_just_pressed("attack") and not is_attacking and not is_jumping:
+		is_attacking = true
+		# Start attack animation
+		play_animation("Attack_" + facing_direction.capitalize())
+
 func apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		in_air = true
 	else:
 		if is_jumping:
 			is_jumping = false
-		in_air = false
 		velocity.y = 0
 
-# Apply movement based on velocity
-func apply_movement():
-	move_and_slide()
-
-# Handle attack input
-func handle_attack():
-	if not is_attacking:
-		is_attacking = true
-		play_animation("Attack_" + facing_direction.capitalize())
-
-# Play a specific animation if it exists
-func play_animation(anim_name: String):
-	if animation_player and animation_player.has_animation(anim_name):
-		if not animation_player.is_playing() or animation_player.current_animation != anim_name:
-			animation_player.play(anim_name)
+func update_animation():
+	if is_attacking:
+		return  # Don't change animation if attacking
+	
+	var anim_to_play = ""
+	if is_jumping:
+		anim_to_play = "Jump_" + facing_direction.capitalize()
+	elif velocity.x != 0:
+		anim_to_play = "Run_" + facing_direction.capitalize() if Input.is_key_pressed(KEY_SHIFT) else "Walk_" + facing_direction.capitalize()
 	else:
-		print("WARNING: Animation '%s' not found in AnimationPlayer!" % anim_name)
+		anim_to_play = "Idle_" + facing_direction.capitalize()
+	
+	play_animation(anim_to_play)
 
-# Handle animation completion
-func _on_animation_finished(anim_name: String):
+func play_animation(anim_name):
+	if animation_player != null:
+		if animation_player.has_animation(anim_name):
+			if animation_player.current_animation != anim_name:
+				animation_player.play(anim_name)
+		else:
+			print("WARNING: Animation '", anim_name, "' not found!")
+	else:
+		print("ERROR: Attempted to play animation '", anim_name, "' but AnimationPlayer is null!")
+
+func _on_animation_finished(anim_name):
 	if anim_name.begins_with("Attack_"):
 		is_attacking = false
-	elif anim_name.begins_with("Jump_") and not in_air:
-		# Transition to idle or walk animation after landing
-		if velocity.x != 0:
-			play_animation("Walk_" + facing_direction.capitalize())
-		else:
-			play_animation("Idle_" + facing_direction.capitalize())
-
-# Function to handle Link getting attacked by Gibdo
-func _on_gibdo_attack():
-	print("Link was attacked by Gibdo.")
-	# Add damage handling logic here if needed in the future
