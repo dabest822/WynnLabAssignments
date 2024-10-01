7cccefd9c887ccfa5d886a8a9437b92e6c9ed512
dabest822 <dembrosdoe@gmail.com>
2024-10-01 05:51:30 -0400

    october 1

--- lab-1-wynn-casadona/Link Everything.gd
+++ lab-1-wynn-casadona/Link Everything.gd
@@ -8,14 +8,13 @@ var facing_direction = "right"
 var is_attacking = false
 var is_jumping = false
 var animation_player: AnimationPlayer
+var jump_animation_played = false
 
 func _ready():
 	print("Link script starting...")
 	animation_player = find_animation_player(self)
-	
 	if animation_player:
 		print("AnimationPlayer found at path: ", animation_player.get_path())
-		# Connect the "animation_finished" signal correctly
 		animation_player.connect("animation_finished", Callable(self, "_on_animation_finished"))
 	else:
 		print("ERROR: AnimationPlayer not found in the scene tree!")
@@ -47,14 +46,15 @@ func handle_input(_delta):
 			velocity.x = -current_speed
 			facing_direction = "left"
 
-	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_attacking:
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_jumping:
 		velocity.y = jump_force
 		is_jumping = true
+		jump_animation_played = false
+		play_animation("Jump_" + facing_direction.capitalize())
 
-	if Input.is_action_just_pressed("attack") and not is_attacking and not is_jumping:
+	if Input.is_action_just_pressed("attack") and not is_attacking and is_on_floor():
 		is_attacking = true
-		# Start attack animation
-		play_animation("Attack_" + facing_direction.capitalize())
+		play_animation("Hit_" + facing_direction.capitalize())
 
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -62,26 +62,32 @@ func apply_gravity(delta):
 	else:
 		if is_jumping:
 			is_jumping = false
-		velocity.y = 0
+			jump_animation_played = false
 
 func update_animation():
 	if is_attacking:
-		return  # Don't change animation if attacking
-	
+		return
+
 	var anim_to_play = ""
+
 	if is_jumping:
-		anim_to_play = "Jump_" + facing_direction.capitalize()
+		if not jump_animation_played:
+			anim_to_play = "Jump_" + facing_direction.capitalize()
+			jump_animation_played = true
+		else:
+			return  # Keep last frame of jump animation until landing or switching animation
 	elif velocity.x != 0:
 		anim_to_play = "Run_" + facing_direction.capitalize() if Input.is_key_pressed(KEY_SHIFT) else "Walk_" + facing_direction.capitalize()
 	else:
 		anim_to_play = "Idle_" + facing_direction.capitalize()
-	
-	play_animation(anim_to_play)
+
+	if anim_to_play != "":
+		play_animation(anim_to_play)
 
 func play_animation(anim_name):
 	if animation_player != null:
 		if animation_player.has_animation(anim_name):
-			if animation_player.current_animation != anim_name:
+			if animation_player.current_animation != anim_name or not animation_player.is_playing():
 				animation_player.play(anim_name)
 		else:
 			print("WARNING: Animation '", anim_name, "' not found!")
@@ -89,5 +95,9 @@ func play_animation(anim_name):
 		print("ERROR: Attempted to play animation '", anim_name, "' but AnimationPlayer is null!")
 
 func _on_animation_finished(anim_name):
-	if anim_name.begins_with("Attack_"):
+	if anim_name.begins_with("Hit_"):
 		is_attacking = false
+	elif anim_name.begins_with("Jump_"):
+		# After jump animation ends, stay in the last frame until new animation is triggered
+		if is_on_floor():
+			play_animation("Idle_" + facing_direction.capitalize())
